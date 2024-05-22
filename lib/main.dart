4f70150f906837572f2855d77dd4a3f891431393
Kanishka Rathnayake <kanishkadilshan861@gmail.com>
2024-05-22 14:41:35 +0530

    sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:digital_clinic_final/Custom_Buttons/custom_buttom_2.dart';
 import 'package:digital_clinic_final/Custom_Buttons/custom_button_1.dart';
 import 'package:digital_clinic_final/Custom_Buttons/custom_button_3.dart';
 import 'package:digital_clinic_final/Custom_Buttons/custom_button_4.dart';
+import 'package:digital_clinic_final/Login_and_Signup/signup_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,44 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SignUp(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool isRadioSelected = false;
-
-  void _toggleRadioButton() {
-    setState(() {
-      isRadioSelected = !isRadioSelected;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      backgroundColor: Colors.pink,
-      body: Center(
-        child: CustomButton2(
-          borderColor: Colors.white,
-          textColor: Colors.white,
-          fillColor: Colors.white38,
-          text: 'Click Me',
-          onPress: _toggleRadioButton,
-          isSelected: isRadioSelected,
-        ),
-      ),
-    );
-  }
-}
