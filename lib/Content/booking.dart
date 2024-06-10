d6b4210920180e2c9eb639f334356b07b7a57493
Kanishka Rathnayake <kanishkadilshan861@gmail.com>
2024-06-11 01:01:01 +0530

    booking page

--- lib/Content/booking.dart
+++ lib/Content/booking.dart
@@ -1,3 +1,5 @@
+import 'package:digital_clinic_final/Custom_Buttons/custom_button_1.dart';
+import 'package:digital_clinic_final/Custom_Buttons/custom_button_3.dart';
 import 'package:flutter/material.dart';
 
 class BookingPage extends StatefulWidget {
@@ -14,6 +16,8 @@ class _BookingPageState extends State<BookingPage> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
     return DefaultTabController(
       length: 3,
       child: Scaffold(
@@ -51,8 +55,224 @@ class _BookingPageState extends State<BookingPage> {
         ),
         body: TabBarView(
           children: [
-            Center(child: Text("Details Tab")),
-            Center(child: Text("Finish Tab")),
+            // Time tab
+            Center(child: Text("Time Tab")),
+
+            // Details tab
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Set type of visit",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: _type1Checked,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _type1Checked = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Video Call",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: _type2Checked,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _type2Checked = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Audio Call",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Divider(thickness: 2),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Do you have Insurance?",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: DropdownButtonFormField<String>(
+                      value: _selectedInsuranceOption,
+                      items: [
+                        DropdownMenuItem(
+                          value: "Select one",
+                          child: Text("Select one"),
+                        ),
+                        DropdownMenuItem(
+                          value: "Yes",
+                          child: Text("Yes"),
+                        ),
+                        DropdownMenuItem(
+                          value: "No",
+                          child: Text("No"),
+                        ),
+                      ],
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedInsuranceOption = newValue!;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You may upload related EHR files, if you wish",
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "These files will only be available for your doctor",
+                          style: TextStyle(fontSize: 14, color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(thickness: 2),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Have you visit this doctor before",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: _type1Checked,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _type1Checked = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Yes",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: _type2Checked,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _type2Checked = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "No",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Divider(thickness: 2),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Payment Process",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: _type1Checked,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _type1Checked = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "After Visit",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: _type2Checked,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _type2Checked = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Before Visit",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: height * 0.01),
+                  CustomButton1(
+                    text: "NEXT",
+                    fillColor: Color.fromRGBO(106, 121, 213, 1.0),
+                    textColor: Colors.white,
+                    borderColor: Colors.transparent,
+                    onPress: () {
+                      // Define your onPress function here
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            // Finish tab
             Center(child: Text("Finish Tab")),
           ],
         ),
